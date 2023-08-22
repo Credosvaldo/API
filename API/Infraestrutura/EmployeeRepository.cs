b71fd672b348a89aee8a1094dc3685b0fc627655
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2023-08-22 10:21:43 -0300

    EmployeeRepository implementado

---
+++ API/Infraestrutura/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using API.Models;
+
+namespace API.Infraestrutura
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ConnectionContex _contex = new ConnectionContex();
+
+        public void Add(Employee employee)
+        {
+            _contex.Employees.Add(employee);
+            _contex.SaveChanges();
+        }
+
+        public List<Employee> Get()
+        {
+            return _contex.Employees.ToList();
+        }
+    }
+}
