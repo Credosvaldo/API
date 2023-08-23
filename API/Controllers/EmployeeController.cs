dd1347ac4102e77f50d70490590c98743a2bca10
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2023-08-23 11:37:14 -0300

    Http Get employee completo

---
+++ API/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using API.Models;
+using API.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public IActionResult Add(EmployeeViewModel employeeViewModel)
+        {
+            var employ = new Employee(employeeViewModel.Name, employeeViewModel.Age, null);
+            _employeeRepository.Add(employ);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employss = _employeeRepository.Get();
+            return Ok(employss);
+        }
+    
+    }
+
+}
